486a60908dee69fb8c39888983e41c386be09918
sharmayash <sharmayash44ys@gmail.com>
2024-01-08 00:31:35 +0530

    Chapter 16: Added tags with next.js metadata API

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,4 +1,4 @@
-import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
@@ -8,6 +8,12 @@ import {
   RevenueChartSkeleton,
   LatestInvoicesSkeleton,
 } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default async function Page() {
   return (
